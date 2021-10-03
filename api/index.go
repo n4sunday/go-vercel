43233d2b7f8416bff97cfcde63398fa885e88dd8
Sunday <dev.nattapon.s@gmail.com>
2021-10-04 03:31:42 +0700

    :tada: initial project

---
+++ api/index.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello World")
+}
+
+func hello2(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello World2")
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.GET("/api/", hello)
+	e.GET("/api/2", hello2)
+
+	e.ServeHTTP(w, r)
+}
